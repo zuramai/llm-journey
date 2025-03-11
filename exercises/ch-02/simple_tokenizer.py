ef09980b2d3ab3e57e04bae74a5d648d9ce662fc
zuramai <saugi.dev@gmail.com>
2025-03-11 11:12:32 +0700

    ch02: add simple tokenizer v2

--- exercises/ch-02/simple_tokenizer.py
+++ exercises/ch-02/simple_tokenizer.py
@@ -13,3 +13,19 @@ class SimpleTokenizerV1:
         text = " ".join([self.int_to_str[i] for i in ids])
         text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
         return text
+    
+
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+    def encode(self, text):
+        result = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        result = [item.strip() for item in result if item.strip()]
+        result = [item if item in self.str_to_int else "<|unk|>" for item in result]
+        ids = [self.str_to_int[s] for s in result]
+        return ids
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
+        return text
